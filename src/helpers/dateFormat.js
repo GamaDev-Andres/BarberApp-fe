6576c89979254dff3975685622158283dcfb40af
Andres Gama <gama_pipe@hotmail.com>
2022-02-27 19:08:10 -0500

    refactorizando dateFormat y arreglando bug

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,8 +1,5 @@
 export const dateFormat = (date) => {
   const fecha = date ? new Date(date) : new Date();
-  const year = fecha.getFullYear();
-  const month =
-    +fecha.getMonth() < 10 ? `0${fecha.getMonth()}` : fecha.getMonth();
-  const day = +fecha.getDate() < 10 ? `0${fecha.getDate()}` : fecha.getDate();
-  return `${year}-${+month + 1 === 1 ? "01" : +month + 1}-${day}`;
+  const fechaFormat = new Intl.DateTimeFormat("fr-CA").format(fecha);
+  return fechaFormat;
 };
